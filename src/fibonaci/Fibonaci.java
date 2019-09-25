8bbfc2da9e819bfd22635553e30b9be07d63740a
BilalMF13 <fakihm@sheridancollege.ca>
2019-09-24 21:04:49 -0400

    Merge origin/master
    Conflicts:
    	src/ca/sheridancollege/week1/softwarefundamentals/StudentTester.java

---
+++ src/fibonaci/Fibonaci.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonaci;
+
+/**
+ *
+ * @author fakih
+ */
+public class Fibonaci {
+    public static void main(String[] args) {
+        
+        int [] fib = new int [20];
+        
+        fib[0] = 0;
+        fib[1] = 1;
+        
+        System.out.printf("0     " + "%05d\n",fib[0]);
+        System.out.printf("1     " + "%05d\n",fib[1]);
+        
+        for(int i = 2; i < fib.length; i++)
+        {
+            fib[i] = fib[i-2] + fib[i-1];
+                        
+            System.out.printf(i + "     " +"%05d\n", fib[i]);
+        }//ends loop
+        
+    }//ends main
+}//ends class
